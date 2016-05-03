67edc55840936d3e819183065ac7e6e02a1783ad
Troy Cox <troyacox@gmail.com>
2016-05-03 17:22:00 +0000

    before action switched

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :require_admin, only: [:oakland, :shadyside]
-  before_action :require_owner_event, only: [:edit, :update, :destroy]
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :require_owner_event, only: [:edit, :update, :destroy]
 
   # GET /events
   # GET /events.json
