6d1d17f7a88a36849db29030bddc38d8e1deaba7
Troy Cox <troyacox@gmail.com>
2016-05-25 20:18:51 +0000

    verification on modal

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
 scope :special_like, -> (special) { where("special ilike ?", special)}
   belongs_to :venue
 
- 
+ def event_venue
+  Venue.where(id: self.venue_id).first
+ end
 end
 
