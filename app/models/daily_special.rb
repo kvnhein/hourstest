52f0d9f896be1301871ea37920f88c9d31be739a
Troy Cox <troyacox@gmail.com>
2016-08-02 08:21:58 +0000

    back to normal

--- app/models/daily_special.rb
+++ app/models/daily_special.rb
@@ -4,7 +4,14 @@ class DailySpecial < ActiveRecord::Base
   acts_as_votable
 
   has_attached_file :image,
-  styles: { :medium => {:geometry => "500x500>", :quality => 100} , thumb: "100x100>" }
+  styles: { :medium => {:geometry => "500x500>", :quality => 100} , thumb: "100x100>" },
+  :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 
