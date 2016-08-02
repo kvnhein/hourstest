0dfe8fac5ba21b91f27df72c4f2042ff10f31592
Troy Cox <troyacox@gmail.com>
2016-08-02 07:07:52 +0000

    test

--- app/models/daily_special.rb
+++ app/models/daily_special.rb
@@ -3,17 +3,17 @@ class DailySpecial < ActiveRecord::Base
   belongs_to :venue
   acts_as_votable
 
-  has_attached_file :image, 
+  has_attached_file :image,
   styles: { :medium => {:geometry => "500x500>", :quality => 100} , thumb: "100x100>" },
   :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
+      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  
+
 
   def event_type
     Venue.where(id: self.venue_id).first.genre unless Venue.where(id: self.venue_id).first.genre.nil?
