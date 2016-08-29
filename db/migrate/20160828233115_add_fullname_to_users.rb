cbf9735946412ea85b0de41bbfefc25ef3a5c049
Troy Cox <troyacox@gmail.com>
2016-08-29 00:08:03 +0000

    trying

--- db/migrate/20160828233115_add_fullname_to_users.rb
+++ db/migrate/20160828233115_add_fullname_to_users.rb
@@ -1,5 +1,3 @@
 class AddFullnameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :fullname, :string
-  end
+
 end
--- db/migrate/20160828233739_add_image_to_users.rb
+++ db/migrate/20160828233739_add_image_to_users.rb
@@ -1,7 +1,3 @@
 class AddImageToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :image, :string
-  end
+  
 end
