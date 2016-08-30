c18ef6fe69d3a16b0caba7df46316d7884b6e6a9
Troy Cox <troyacox@gmail.com>
2016-08-30 17:02:23 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   devise_for 	:users,
   						:path => '',
   						:path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
-  						:controllers => {:omniauth_callbacks => 'omniauth_callbacks',
-  														 :registrations => 'registrations'
+  						:controllers => {:omniauth_callbacks => 'omniauth_callbacks'
   														}
 
   get 'tags/:tag', to: 'daily_specials#index', as: :tag
