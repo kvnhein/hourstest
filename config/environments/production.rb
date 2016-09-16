e0fa54e58dd06f051969c5aa606904828ac9dae6
Troy Cox <troyacox@gmail.com>
2016-09-16 17:55:30 +0000

    good color

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "gmail.com",
+  domain: ENV["GMAIL_DOMAIN"],
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: "hourspgh",
-  password: "GathrGroup"
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
   }
 
   # Use a different cache store in production.
