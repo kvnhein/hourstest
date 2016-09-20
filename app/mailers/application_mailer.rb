4c693bc0d05c95caec4f3954c2cb100984807cc4
Troy Cox <troyacox@gmail.com>
2016-09-20 06:52:58 +0000

    time

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "hourspgh@gmail.com"
   layout 'mailer'
-  
+
   def sample_email(user, hour)
     @user = user
     @hour = hour
-    mail(to: @user.email, subject: '#{Venue.find(@hour.venue_id).name} has posted a new Hour')
+    mail(to: @user.email, subject: 'Hours Reminder')
   end
 end
