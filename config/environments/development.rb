2d2f518490d49247140bb808c464b077779139a9
Troy Cox <troyacox@gmail.com>
2016-10-07 16:54:58 +0000

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: "hourspgh",
-  password: "GathrGroupHours"
+  password: "GathrGroupHoursPGH"
 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
